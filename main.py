5af329a6efb131e8fc009e5d7102e5295e1d8cb3
tevis-trtr <alansilva20550@gmail.com>
2026-02-18 01:12:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def responder_ia(ctx, pergunta):
     ] + memoria[ctx.author.id]
 
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        llama-3.1-70b-versatile
         messages=messages,
         temperature=0.7,
         max_tokens=800
@@ -188,5 +188,6 @@ async def ban(ctx, member: discord.Member, *, reason=None):
 # ==============================
 
 bot.run(TOKEN)
+
 
 
